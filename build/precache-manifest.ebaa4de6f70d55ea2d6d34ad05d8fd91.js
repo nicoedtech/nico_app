a8e48325075df5d14dbf5c38abd6e457e2af3ab2
Sarah Akbar <sarah_akbar@bose.com>
2019-08-14 09:17:57 -0400

    Basic extension

---
+++ build/precache-manifest.ebaa4de6f70d55ea2d6d34ad05d8fd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccab5fc936764dad4f69b7c5fa98ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae2c1d351c436480611",
+    "url": "/static/css/main.52e25909.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9ae2c1d351c436480611",
+    "url": "/static/js/main.cfcb3240.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e73bbe23550e92c6a6f58c79b3092b08",
+    "url": "/static/media/cat.e73bbe23.gif"
+  }
+]);
